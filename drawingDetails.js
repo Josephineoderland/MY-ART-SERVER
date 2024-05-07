915a46fb0468bc76dd151e85d52b111dc122dd2d
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-07 12:53:54 +0200

    Initial commit

---
+++ drawingDetails.js
@@ -0,0 +1,55 @@
+const data = {
+  hair: {
+    length: ["Short", "Medium-length", "Long"],
+    type: ["Straight", "Curly", "Wavy"],
+    color: ["Blonde", "Brown", "Black", "Red", "Gray", "Pink"],
+    texture: ["Shiny", "Matte", "Rough"],
+    style: ["Updo", "Down", "Braids", "Ponytails", "Bangs"],
+    accessories: ["Hats", "Headbands", "Clips", "Tiaras"]
+  },
+  faceShape: {
+    shape: ["Oval", "Round", "Square", "Heart-shaped"],
+    chin: ["Pointed", "Rounded", "Defined", "Smooth"],
+    cheekbones: ["Prominent", "Subtle", "High-set", "Low-set"],
+    forehead: ["Wide", "Narrow", "High", "Low"],
+    jawline: ["Defined", "Soft", "Straight", "V-shaped"]
+  },
+  nose: {
+    width: ["Wide", "Narrow", "Medium-width"],
+    length: ["Short", "Medium-length", "Long"],
+    shape: ["Straight", "Curved", "Crooked", "Button nose"],
+    nostrils: ["Large", "Small", "Pronounced", "Subtle"]
+  },
+  eyes: {
+    shape: ["Almond", "Droopy", "Almond-shaped", "Round"],
+    eyelids: ["Open", "Half-open", "Closed", "Sleeping"],
+    eyelashes: ["Long", "Short", "Thick", "Separated"]
+  },
+  mouth: {
+    width: ["Narrow", "Wide"],
+    lipShape: ["Full", "Thin", "Asymmetrical"],
+    expression: ["Tightly closed", "Partially open", "Smiling", "Wrinkled"],
+    naturalShape: ["Heart-shaped", "Round", "Square", "Triangular"],
+    corners: ["Upturned", "Downturned", "Neutral"]
+  },
+  posture: {
+    bodyPosition: ["Upright", "Leaning forward", "Leaning backward"],
+    shoulders: ["Raised", "Lowered", "Level"],
+    headAngle: ["Straight ahead", "Turned sideways", "Tilted"]
+  },
+  clothing: {
+    style: ["Classic", "Modern", "Retro", "Sporty", "Elegant", "Casual"],
+    colors: ["Light", "Dark", "Pastel", "Neon", "Solid", "Patterned"],
+    patterns: ["Striped", "Checkered", "Floral", "Animal print", "Geometric"],
+    accessories: ["Necklaces", "Bracelets", "Hats", "Glasses", "Bags", "Shoes"]
+  },
+  physicalFeatures: {
+    freckles: ["Few", "Many", "Scattered", "Concentrated in certain areas"],
+    wrinkles: ["Deep", "Superficial", "Around the eyes", "Around the mouth", "On the forehead"]
+  },
+  age: {
+    howOld: ["kid", "Old", "adult"]
+  }
+};
+
+export default data;
