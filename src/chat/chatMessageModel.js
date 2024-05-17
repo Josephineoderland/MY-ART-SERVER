0f562c7498eb9fa8b7722d0fa5063120f998eaa1
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-17 13:41:04 +0200

    img

--- src/chat/chatMessageModel.js
+++ src/chat/chatMessageModel.js
@@ -4,7 +4,7 @@ const chatMessageSchema = new mongoose.Schema({
   text: { type: String, required: true },
   image: String,
   createdAt: { type: Date, default: Date.now },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  likes: { type: Number, default: 0 },
 })
 
 const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema)
