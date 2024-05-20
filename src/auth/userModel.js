55540e9f46cdf7ca3771e153a01940c50be81fab
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-20 15:31:04 +0200

    chat

--- src/auth/userModel.js
+++ src/auth/userModel.js
@@ -1,21 +1,22 @@
-import mongoose from "mongoose"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-})
+});
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next()
-  this.password = await bcrypt.hash(this.password, 10)
-  next()
-})
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 userSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password)
-}
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
