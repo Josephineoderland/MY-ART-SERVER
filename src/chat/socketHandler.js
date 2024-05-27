0be282fe339e1e34aba7ca157c14d9fe9ece8fb1
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-27 10:32:24 +0200

    cors

--- src/chat/socketHandler.js
+++ src/chat/socketHandler.js
@@ -4,7 +4,7 @@ import ChatMessage from "./chatMessageModel.js";
 function initializeSocket(server) {
   const io = new Server(server);
 
-  io.of("/chat").on("connection", (socket) => {
+  io.of("/messages").on("connection", (socket) => {
     console.log("A user connected to chat");
     
     socket.on("chatMessage", async (data) => {
@@ -12,7 +12,7 @@ function initializeSocket(server) {
       try {
         const newMessage = new ChatMessage({ text: data.text, user: data.user });
         await newMessage.save();
-        io.of("/chat").emit("chatMessage", newMessage);
+        io.of("/messages").emit("chatMessage", newMessage);
       } catch (error) {
         console.error("Error saving chat message:", error);
       }
