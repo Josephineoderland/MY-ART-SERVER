e545a871772a8dc787799fa3e50cb7d6e8d07150
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-27 09:55:38 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import cors from "cors"
+import initializeSocket from "./chat/socketHandler.js"
+import http from "http"
 import listEndpoints from "express-list-endpoints"
 import apiRouter from "./api/api.js"
 import chatRouter from "./chat/chat.js"
@@ -10,7 +12,6 @@ import friendRequestRouter from "./auth/friendRequestRouter.js"
 import userRouter from "./auth/userRouter.js"
 const app = express()
 const port = process.env.SERVER_PORT || 3002
-
 
 app.use(
   cors({
@@ -41,6 +42,9 @@ app.use("/auth", authRouter)
 app.use("/search", searchRouter)
 app.use("/friends", friendRequestRouter)
 app.use("/userId", userRouter)
+
+const server = http.createServer(app)
+initializeSocket(server)
 
 app.listen(port, () => {
   console.log(`Chattservern lyssnar på port ${port}`)
