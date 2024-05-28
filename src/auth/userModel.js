82430687a3ed8be5331959c638842be5959bff8e
Josephineoderland <153084500+Josephineoderland@users.noreply.github.com>
2024-05-28 11:50:49 +0200

    friend

--- src/auth/userModel.js
+++ src/auth/userModel.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs"
 import mongoose from "mongoose"
 
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
--- src/chat/chatMessageModel.js
+++ src/chat/chatMessageModel.js
@@ -1,11 +1,10 @@
-
 import mongoose from "mongoose"
 
 const chatMessageSchema = new mongoose.Schema({
   text: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   likes: { type: Number, default: 0 },
-  user: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, 
 })
 
 const ChatMessage = mongoose.model("ChatMessage", chatMessageSchema)
